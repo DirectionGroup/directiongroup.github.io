9ff06bf9c2765f11a62abc9030d7c04502de2138
iampeterbanjo <peterbanjo@gmail.com>
2016-01-14 17:28:17 +0000

    Add svg animation

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
+var dotMapSvg = document.getElementById('dot-map-svg')
+		, pathLine = dotMapSvg.getAttribute('data-line')
+		, polylines = Snap(dotMapSvg).selectAll('polyline')
+		, lines = Snap(dotMapSvg).selectAll('line')
+		, circles = Snap(dotMapSvg).selectAll('circle')
+		, allLines = Snap(dotMapSvg).selectAll('polyline, line');
+		;
+		
+function forEach(items, func) {
+	for (var index = 0; index < items.length; index++) {
+		var element = items[index];
+		
+		func(element);
+	}
+}
+
+function getRandomInt(min, max){
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomToggleFade() {
+	var item = circles[getRandomInt(0, circles.length - 1)];
+	
+	item.toggleClass('variant');
+}
+
+setInterval(randomToggleFade, 100);
+
 $(document).foundation();
